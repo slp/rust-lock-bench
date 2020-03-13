7907e6990d9cbf68a482a4764744e19906fd35e1
Sergio Lopez <slp@redhat.com>
2020-03-13 17:37:28 +0100

    Include a syscall in the critical section

--- src/lock_bench.rs
+++ src/lock_bench.rs
@@ -1,6 +1,7 @@
-use std::thread;
+use std::path::Path;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{Ordering, AtomicUsize};
+use std::thread;
 
 use std::sync::RwLock as StdRwLock;
 extern crate parking_lot;
@@ -8,8 +9,16 @@ use parking_lot::RwLock as ParkingRwLock;
 
 const N: usize = 10000;
 
-pub fn with<T, G>(num_reader: usize, num_writer: usize, gen: G, write: fn(&Arc<T>), read: fn(&Arc<T>))
-    where T: Sync + Send + 'static, G: Fn() -> T {
+pub fn with<T, G>(
+    num_reader: usize,
+    num_writer: usize,
+    gen: G,
+    write: fn(&Arc<T>),
+    read: fn(&Arc<T>),
+) where
+    T: Sync + Send + 'static,
+    G: Fn() -> T,
+{
     let mut ths = Vec::new();
     let counter = Arc::new(gen());
 
@@ -38,12 +47,17 @@ pub fn with<T, G>(num_reader: usize, num_writer: usize, gen: G, write: fn(&Arc<T
 
 pub fn with_mutex(num_reader: usize, num_writer: usize) {
     fn write(counter: &Arc<Mutex<usize>>) {
-         let mut counter_ref = counter.lock().unwrap();
-         *counter_ref += 1;
+        let mut counter_ref = counter.lock().unwrap();
+        if !Path::new("does_not_exist.txt").exists() {
+            *counter_ref += 1;
+        }
     }
 
     fn read(counter: &Arc<Mutex<usize>>) {
-         let _ = counter.lock().unwrap();
+        let c = counter.lock().unwrap();
+        if Path::new("does_not_exist.txt").exists() {
+            println!("count: {:?}", c);
+        }
     }
 
     with(num_reader, num_writer, || Mutex::new(0), write, read);
@@ -52,18 +66,22 @@ pub fn with_mutex(num_reader: usize, num_writer: usize) {
 pub fn with_rwlock(num_reader: usize, num_writer: usize) {
     fn write(counter: &Arc<StdRwLock<usize>>) {
         let mut counter_ref = counter.write().unwrap();
-        *counter_ref += 1;
+        if !Path::new("does_not_exist.txt").exists() {
+            *counter_ref += 1;
+        }
     }
 
     fn read(counter: &Arc<StdRwLock<usize>>) {
-        let _ = counter.read().unwrap();
+        let c = counter.read().unwrap();
+        if Path::new("does_not_exist.txt").exists() {
+            println!("count: {:?}", c);
+        }
     }
 
     with(num_reader, num_writer, || StdRwLock::new(0), write, read);
 }
 
 pub fn with_parking_rwlock(num_reader: usize, num_writer: usize) {
-    ;
     fn write(counter: &Arc<ParkingRwLock<usize>>) {
         let mut counter_ref = counter.write();
         *counter_ref += 1;
@@ -73,7 +91,13 @@ pub fn with_parking_rwlock(num_reader: usize, num_writer: usize) {
         let _ = counter.read();
     }
 
-    with(num_reader, num_writer, || ParkingRwLock::new(0), write, read);
+    with(
+        num_reader,
+        num_writer,
+        || ParkingRwLock::new(0),
+        write,
+        read,
+    );
 }
 
 pub fn with_atomic_seq_cst(num_reader: usize, num_writer: usize) {
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ extern crate time;
 extern crate lock_bench;
 
 use statrs::statistics::Statistics;
-use time::{PreciseTime};
+use time::PreciseTime;
 
 use lock_bench::*;
 
-const N : i32 = 8;
+const N: i32 = 8;
 
 fn run(f: &Fn()) -> f64 {
     let start = PreciseTime::now();
@@ -19,37 +19,44 @@ fn run(f: &Fn()) -> f64 {
 }
 
 fn print_result(label: &str, result: &Vec<f64>) {
-    println!("{:8} =>   mean: {:8.2}, std_dev: {:8.2}",
-             label, result.clone().mean(), result.clone().std_dev());
+    println!(
+        "{:8} =>   mean: {:8.2}, std_dev: {:8.2}",
+        label,
+        result.clone().mean(),
+        result.clone().std_dev()
+    );
 }
 
 pub fn iter(num_reader: usize, num_writer: usize) {
-    println!("==== num_reader: {}, num_writer: {} ====", num_reader, num_writer);
+    println!(
+        "==== num_reader: {}, num_writer: {} ====",
+        num_reader, num_writer
+    );
 
     let mut bm_mutex = Vec::new();
     let mut bm_rwlock = Vec::new();
-    let mut bm_parking_rwlock = Vec::new();
-    let mut bm_atomic_seq_cst = Vec::new();
-    let mut bm_atomic_relaxed = Vec::new();
-    let mut bm_unsynchronized = Vec::new();
+    //let mut bm_parking_rwlock = Vec::new();
+    //let mut bm_atomic_seq_cst = Vec::new();
+    //let mut bm_atomic_relaxed = Vec::new();
+    //let mut bm_unsynchronized = Vec::new();
 
     for i in 0..N {
         if i > 0 {
             bm_mutex.push(run(&|| with_mutex(num_reader, num_writer)));
             bm_rwlock.push(run(&|| with_rwlock(num_reader, num_writer)));
-            bm_parking_rwlock.push(run(&|| with_parking_rwlock(num_reader, num_writer)));
-            bm_atomic_seq_cst.push(run(&|| with_atomic_seq_cst(num_reader, num_writer)));
-            bm_atomic_relaxed.push(run(&|| with_atomic_relaxed(num_reader, num_writer)));
-            bm_unsynchronized.push(run(&|| with_unsynchronized(num_reader, num_writer)));
+            //bm_parking_rwlock.push(run(&|| with_parking_rwlock(num_reader, num_writer)));
+            //bm_atomic_seq_cst.push(run(&|| with_atomic_seq_cst(num_reader, num_writer)));
+            //bm_atomic_relaxed.push(run(&|| with_atomic_relaxed(num_reader, num_writer)));
+            //bm_unsynchronized.push(run(&|| with_unsynchronized(num_reader, num_writer)));
         }
     }
 
     print_result("mutex", &bm_mutex);
     print_result("rwlock", &bm_rwlock);
-    print_result("prwlock", &bm_rwlock);
-    print_result("seq_cst", &bm_atomic_seq_cst);
-    print_result("relaxed", &bm_atomic_relaxed);
-    print_result("unsync", &bm_unsynchronized);
+    //print_result("prwlock", &bm_parking_rwlock);
+    //print_result("seq_cst", &bm_atomic_seq_cst);
+    //print_result("relaxed", &bm_atomic_relaxed);
+    //print_result("unsync", &bm_unsynchronized);
     println!("");
 }
 
